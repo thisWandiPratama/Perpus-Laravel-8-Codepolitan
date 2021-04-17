4f3460511029635ece1aa9165ea63061e760283f
thisWandiPratama <wandi99pratama@gmail.com>
2021-04-17 21:40:36 +0700

    PerPus Implementasi SweetAlert Detele Author

--- resources/views/admin/author/action.blade.php
+++ resources/views/admin/author/action.blade.php
@@ -1,6 +1,7 @@
 <a href="{{route('admin.auhtor.edit', $model) }}" class="btn btn-warning">Edit</a>
 <button href="{{route('admin.auhtor.destroy', $model) }}" class="btn btn-danger" id="delete">Hapus</button>
 
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script>
 
     $("button#delete").on('click', function(e) {
@@ -9,8 +10,24 @@
         var href = $("#delete").attr("href");
         console.log(href)
 
-        document.getElementById('deleteForm').action = href;
-        document.getElementById('deleteForm').submit();
-    })
+        swal({
+            title: "Apakah kamu yakin ingin hapus data ini?",
+            text: "Data akan terhapus secara permanen!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+            })
+            .then((willDelete) => {
+            if (willDelete) {
+                document.getElementById('deleteForm').action = href;
+                document.getElementById('deleteForm').submit();
+                swal("Berhasil! Data telah terhapus!", {
+                icon: "success",
+                });
+            } else {
+                swal("Data kamu masih aman!");
+            }
+    });
+})
 </script>
 
