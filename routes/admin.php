ac323d38e15d95ce6bcc190d3c627b2b94e83cd1
thisWandiPratama <wandi99pratama@gmail.com>
2021-04-22 10:55:58 +0700

    implementasi Route Resource

--- routes/admin.php
+++ routes/admin.php
@@ -4,11 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/','HomeController@index')->name('dashboard');
 
-Route::get('/author','AuthorController@index')->name('author.index');
-Route::get('/author/create','AuthorController@create')->name('author.create');
-Route::post('/author/store','AuthorController@store')->name('author.store');
-Route::get('/author/{author}/edit','AuthorController@edit')->name('author.edit');
-Route::put('/author/{author}','AuthorController@update')->name('author.update');
-Route::delete('/author/{author}','AuthorController@destroy')->name('author.destroy');
+// Route::get('/author','AuthorController@index')->name('author.index');
+// Route::get('/author/create','AuthorController@create')->name('author.create');
+// Route::post('/author/store','AuthorController@store')->name('author.store');
+// Route::get('/author/{author}/edit','AuthorController@edit')->name('author.edit');
+// Route::put('/author/{author}','AuthorController@update')->name('author.update');
+// Route::delete('/author/{author}','AuthorController@destroy')->name('author.destroy');
 Route::get('/author/data','DataController@authors')->name('author.data');
+Route::resource('author', 'AuthorController');
 
